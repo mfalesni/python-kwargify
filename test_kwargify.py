1334749ac1a0220076b91f335fb1b24ca6cb60be
Milan Falešník <mfalesni@redhat.com>
2015-01-26 14:47:38 +0100

    Make the kwargifier work on methods too.
    The limitation here is that only existing methods can be used (eg. x = kwargify(y.z)) because if we wanted to decorate function to become a kwargified method, the kwarigfy class would have to be a descriptor.

--- test_kwargify.py
+++ test_kwargify.py
@@ -73,3 +73,67 @@ class TestFunctionWithDefaultValues(object):
 
     def test_override_default_with_kwarg(self, function):
         assert function(1, b=2)["b"] == 2
+
+
+class TestKwargifyMethod(object):
+    class _TestClass(object):
+        def noargs(self):
+            return locals()
+
+        def onlyargs(self, a, b):
+            return locals()
+
+        def withdefault(self, a, b=None):
+            return locals()
+
+    @pytest.fixture(scope="class")
+    def o(self):
+        return self._TestClass()
+
+    # No args test
+    def test_no_args_given(self, o):
+        kwargify(o.noargs)()
+
+    @pytest.mark.xfail
+    @pytest.mark.parametrize("n", range(1, 4))
+    def test_args_given(self, o, n):
+        kwargify(o.noargs)(*range(n + 1))
+
+    def test_kwargs_passed(self, o):
+        kwargify(o.noargs)(foo="bar")
+
+    # Only args
+    @pytest.mark.xfail
+    def test_no_args_given_fails(self, o):
+        kwargify(o.onlyargs)()
+
+    @pytest.mark.xfail
+    def test_args_given_not_enough(self, o):
+        kwargify(o.onlyargs)(1)
+
+    def test_args_given_enough(self, o):
+        kwargify(o.onlyargs)(1, 2)
+
+    @pytest.mark.xfail
+    def test_only_kwargs_passed_wrong(self, o):
+        kwargify(o.onlyargs)(foo="bar")
+
+    @pytest.mark.xfail
+    def test_only_kwargs_passed_not_enough(self, o):
+        kwargify(o.onlyargs)(a="bar")
+
+    def test_only_kwargs_passed(self, o):
+        kwargify(o.onlyargs)(a=1, b=2)
+
+    def test_both_passed(self, o):
+        kwargify(o.onlyargs)(1, b=2)
+
+    # Default values
+    def test_pass_only_required(self, o):
+        assert kwargify(o.withdefault)(1)["b"] is None
+
+    def test_override_default_with_arg(self, o):
+        assert kwargify(o.withdefault)(1, 2)["b"] == 2
+
+    def test_override_default_with_kwarg(self, o):
+        assert kwargify(o.withdefault)(1, b=2)["b"] == 2
